620d157c568f2f3bb023a53d414434256ab51dce
Bree Carrick <breecarrick@live.com>
2017-03-17 00:19:31 -0600

    document add and edit forms work

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DocumentsService } from '../documents.service';
+import { Subscription } from 'rxjs/RX';
+import { Document } from '../document';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'cms-document-edit',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
+   private documentIndex: number; //to get the index later
+   subscription: Subscription; // route subscription
+   oldDocument: Document; // ref. to old document
+   editMode: boolean = false; // in edit mode flag?
 
-  constructor() { }
+  constructor(private documentsService: DocumentsService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('idx')) {
+          this.documentIndex = +params['idx'];
+          this.oldDocument = this.documentsService.getDocument(this.documentIndex);
+          this.editMode = true;
+        } else {
+          this.oldDocument = null;
+          this.editMode = false;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onSubmit(value) {
+    let newDocument = new Document(null,
+                                   value.name,
+                                   value.description,
+                                   value.url, null);
+    if (this.editMode) {
+      newDocument.id = this.oldDocument.id;
+      this.documentsService.updateDocument(this.oldDocument, newDocument);
+    } else {
+      this.documentsService.addDocument(newDocument)
+    }
+    this.router.navigate(['documents']);
+  }
+
+  onCancel() {
+    this.router.navigate(['documents']);
   }
 
 }
